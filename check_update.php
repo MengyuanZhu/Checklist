352e01e6f499e80d7fa7b55036c483ff4b082282
Mengyuan Zhu <zhumengyuan@outlook.com>
2016-01-26 14:36:07 -0500

    timespan function added

--- check_update.php
+++ check_update.php
@@ -1,47 +1,56 @@
 <?php
 
-$today = date("D");
-$today_number=date("N");
+$today        = date("D");
+$today_number = date("N");
 ini_set('display_errors', 'on');
 Error_reporting(E_ALL);
-require ("sql.php");
+require "sql.php";
 
-foreach($_POST as $key => $value)
-	{
+foreach ($_POST as $key => $value) {
 
-	$phptime = date('H:i:s', time());
-	$personname = substr($key, strpos($key, "_") + 1, strlen($key));
-  $midnight=substr($key, 0, strpos($key, "_") );
-	$personname = str_replace('_', ' ', $personname);
+    $phptime    = date('H:i:s', time());
+    $personname = substr($key, strpos($key, "_") + 1, strlen($key));
+    $midnight   = substr($key, 0, strpos($key, "_"));
+    $personname = str_replace('_', ' ', $personname);
 
+    if ($midnight == "mid") {
+        $today_number -= 2;
+        $dowMap = array('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun');
 
-  if ($midnight=="mid"){
-    $today_number-=2;
-    $dowMap = array( 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun');
+        $sql    = "UPDATE people SET " . $dowMap[$today_number] . "_out" . "=\"$phptime\" WHERE name='$personname';";
+        $result = $conn->query($sql);
+    } else {
 
-    $sql = "UPDATE people SET " . $dowMap[$today_number] . "_out" . "=\"$phptime\" WHERE name='$personname';";
-    $result = $conn->query($sql);
-  }
+        $state  = substr($key, 0, strpos($key, "_"));
+        $sql    = "select " . $today . "_" . $state . " from people WHERE name='$personname';";
+        $result = $conn->query($sql);
+        $row    = $result->fetch_assoc();
+        if ($row[$today . "_" . $state] == null) {
+            $sql = "UPDATE people SET " . $today . "_" . $state . "=\"$phptime\" WHERE name='$personname';";
+            if ($state == "out") {
+                $sql    = "select " . $today . "_in" . " from people WHERE name='$personname';";
+                $result = $conn->query($sql);
+                $row    = $result->fetch_assoc();
+                $span= (strtotime($phptime) - strtotime($row[$today . "_in"]))/60;
 
-  else{
+                if ($span <5) {
 
+                    echo "<h1>You even did not stay in the lab for 5 minutes...</h1>";
+                    echo " You have been in the lab for ".$span."min<br /><br />";
+                    echo "<img src='shame-on-you.jpg' alt='shameonyou'>";
+                    header("Refresh:3; url=index.php");
+                } else {
+                    $sql    = "UPDATE people SET " . $today . "_" . $state . "=\"$phptime\" WHERE name='$personname';";
+                    $result = $conn->query($sql);
+                    header("Refresh:0; url=index.php");
+                }
 
-	$state = substr($key, 0, strpos($key, "_"));
-	$sql = "select " . $today . "_" . $state . " from people WHERE name='$personname';";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	if ($row[$today . "_" . $state] == NULL)
-		{
-		$sql = "UPDATE people SET " . $today . "_" . $state . "=\"$phptime\" WHERE name='$personname';";
-		$result = $conn->query($sql);
-		}
+            } else {
+                $result = $conn->query($sql);
+                header("Refresh:0; url=index.php");
+            }
+
+        }
 
     }
-	}
-
-
-
-
-header("Refresh:0; url=index.php");
-
-?>
+}
